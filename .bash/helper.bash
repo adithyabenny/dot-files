adithyabenny/dot-files

commit f15606d7c47343e5c3a32d785dc82e6681a5633e

Make the network interface Bash helper function Chromebook compatible

.bash/helper.bash
@@ -108,9 +108,9 @@ pb() {
   fi
 }
 
-# list all interfaces and their IPs
+# list all network interfaces and their IPs
 ipp() {
-  interfaces=$(ifconfig | grep mtu | awk '{print $1}' | cut -d':' -f1 | grep -v 'lo')
+  interfaces=$(ifconfig | awk '{print $1}' RS='' | cut -d':' -f1 | grep -v 'lo')
   for interface in $interfaces ; do
     ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -d':' -f2)
     if [[ -n "$ip" ]] ; then

commit c7399c33a7a236736bb111b5a363bf4aa6a76220

Fix for broken Bash helper function caused by f15606d

.bash/helper.bash
@@ -117,7 +117,7 @@ pb() {
 
 # list all network interfaces and their IPs
 ipp() {
-  interfaces=$(ifconfig | awk '{print $1}' RS='' | cut -d':' -f1 | grep -v 'lo')
+  interfaces=$(ifconfig | grep UP | awk '{print $1}' | cut -d':' -f1 | grep -v 'lo')
   for interface in $interfaces ; do
     ip=$(ifconfig $interface | grep "inet[^6]" | awk '{print $2}' | cut -d':' -f2)
     if [[ -n "$ip" ]] ; then